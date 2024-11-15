ade52c2e6d4862cd911470e05fa44dd78316b5a1
rioowho <riooishere>
2024-11-15 21:18:57 +0700

    api.riooxdzzz

---
+++ index.js
@@ -0,0 +1,338 @@
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+const axios = require('axios');
+const fetch = require('node-fetch');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+app.enable("trust proxy");
+app.set("json spaces", 2);
+
+// Middleware untuk CORS
+app.use(cors());
+
+// Fungsi untuk LuminAI
+async function gptlogic(message, logic) { // Membuat fungsi openai untuk dipanggil
+    let response = await axios.post("https://chateverywhere.app/api/chat/", {
+        "model": {
+            "id": "gpt-4",
+            "name": "GPT-4",
+            "maxLength": 32000,  // Sesuaikan token limit jika diperlukan
+            "tokenLimit": 8000,  // Sesuaikan token limit untuk model GPT-4
+            "completionTokenLimit": 5000,  // Sesuaikan jika diperlukan
+            "deploymentName": "gpt-4"
+        },
+        "messages": [
+            {
+                "pluginId": null,
+                "content": message, 
+                "role": "user"
+            }
+        ],
+        "prompt": logic, 
+        "temperature": 0.5
+    }, { 
+        headers: {
+            "Accept": "/*/",
+            "User-Agent": "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Mobile Safari/537.36"
+        }
+    });
+    
+    let result = response.data;
+    return result;
+}
+
+async function aikurumi(message) {
+    const formData = new FormData();
+    formData.append("locale", 'id-ID');
+    formData.append("content", `<voice name="ja-JP-AoiNeural">${message}</voice>`);
+    formData.append("ip", '46.161.194.33');
+    const response = await fetch('https://app.micmonster.com/restapi/create', {
+        method: 'POST',
+        body: formData
+    });
+    return Buffer.from(('data:audio/mpeg;base64,' + await response.text()).split(',')[1], 'base64');
+};
+const sendToGemini = async (prompt) => {
+    const apiKey = 'AIzaSyD-BIXRyW2O3x4vLTFmfRWIk_pxnMc_SVs'; // Dapatkan apikey dari  https://aistudio.google.com/app/apikey
+    const url = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=${apiKey}`;
+    const body = {
+        contents: [
+            {
+                parts: [
+                    { text: prompt }
+                ]
+            }
+        ]
+    };
+    try {
+        const response = await fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(body)
+        });
+
+        const data = await response.json();
+
+        if (response.ok) {
+            return data; 
+        } else {
+            throw new Error(data.error.message || 'Request failed');
+        }
+    } catch (error) {
+        console.error('Error:', error.message);
+        return null;
+    }
+};
+async function LuminAI(message, model = "gpt-4o-mini") {
+                try {
+                    const response = await axios.post('https://luminai.my.id/v2', {
+                        text: message,
+                        model: model
+                    });
+
+                    return response.data.reply.reply;
+                } catch (error) {
+                    console.error("Terjadi kesalahan:", error.message);
+                    throw new Error("Gagal mendapatkan respons dari AI.");
+                }
+            }
+
+// Fungsi untuk ragBot
+async function ragBot(message) {
+  try {
+    const response = await axios.post('https://ragbot-starter.vercel.app/api/chat', {
+      messages: [{ role: 'user', content: message }],
+      useRag: true,
+      llm: 'gpt-3.5-turbo',
+      similarityMetric: 'cosine'
+    });
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+}
+
+// Fungsi untuk degreeGuru
+async function degreeGuru(message, prompt) {
+  try {
+    const response = await axios.post('https://degreeguru.vercel.app/api/guru', {
+      messages: [
+        { role: 'user', content: message }
+      ]
+    });
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+}
+
+// Fungsi untuk pinecone
+async function pinecone(message) {
+  try {
+    const response = await axios.post('https://pinecone-vercel-example.vercel.app/api/chat', {
+      messages: [{ role: 'user', content: message }]
+    });
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+}
+
+// Fungsi untuk smartContract
+async function smartContract(message) {
+  try {
+    const response = await axios.post("https://smart-contract-gpt.vercel.app/api/chat", {
+      messages: [{ content: message, role: "user" }]
+    });
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function blackboxAIChat(message) {
+  try {
+    const response = await axios.post('https://www.blackbox.ai/api/chat', {
+      messages: [{ id: null, content: message, role: 'user' }],
+      id: null,
+      previewToken: null,
+      userId: null,
+      codeModelMode: true,
+      agentMode: {},
+      trendingAgentMode: {},
+      isMicMode: false,
+      isChromeExt: false,
+      githubToken: null
+    });
+
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+}
+
+// Endpoint untuk servis dokumen HTML
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Endpoint untuk LuminAI
+app.get('/api/luminai', async (req, res) => {
+  try {
+    const { message }= req.query;
+    if (!message) {
+      return res.status(400).json({ error: 'Parameter "message" tidak ditemukan' });
+    }
+    const response = await LuminAI(message);
+    res.status(200).json({
+      status: 200,
+      creator: "RiooXdzz",
+      data: { response }
+    });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// Endpoint untuk ragBot
+app.get('/api/ragbot', async (req, res) => {
+  try {
+    const message = req.query.message;
+    if (!message) {
+      return res.status(400).json({ error: 'Parameter "message" tidak ditemukan' });
+    }
+    const response = await ragBot(message);
+    res.status(200).json({
+      status: 200,
+      creator: "RiooXdzz",
+      data: { response }
+    });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// Endpoint untuk degreeGuru
+app.get('/api/degreeguru', async (req, res) => {
+  try {
+    const { message }= req.query;
+    if (!message) {
+      return res.status(400).json({ error: 'Parameter "message" tidak ditemukan' });
+    }
+    const response = await degreeGuru(message);
+    res.status(200).json({
+      status: 200,
+      creator: "RiooXdzz",
+      data: { response }
+    });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// Endpoint untuk pinecone
+app.get('/api/pinecone', async (req, res) => {
+  try {
+    const message = req.query.message;
+    if (!message) {
+      return res.status(400).json({ error: 'Parameter "message" tidak ditemukan' });
+    }
+    const response = await pinecone(message);
+    res.status(200).json({
+      status: 200,
+      creator: "RiooXdzz",
+      data: { response }
+    });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// Endpoint untuk smartContract
+app.get('/api/smartcontract', async (req, res) => {
+  try {
+    const message = req.query.message;
+    if (!message) {
+      return res.status(400).json({ error: 'Parameter "message" tidak ditemukan' });
+    }
+    const response = await smartContract(message);
+    res.status(200).json({
+      status: 200,
+      creator: "RiooXdzz",
+      data: { response }
+    });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// Endpoint untuk blackboxAIChat
+app.get('/api/blackboxAIChat', async (req, res) => {
+  try {
+    const message = req.query.message;
+    if (!message) {
+      return res.status(400).json({ error: 'Parameter "message" tidak ditemukan' });
+    }
+    const response = await blackboxAIChat(message);
+    res.status(200).json({
+      status: 200,
+      creator: "RiooXdzz",
+      data: { response }
+    });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+app.get('/api/gptlogic', async (req, res) => {
+  try {
+    const message = req.query.message;
+    if (!message) {
+      return res.status(400).json({ error: 'Parameter "message" tidak ditemukan' });
+    }
+    const response = await gptlogic(message);
+    res.status(200).json({
+      status: 200,
+      creator: "RiooXdzz",
+      data: { response }
+    });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+app.get('/api/aikurumi', async (req, res) => {
+  try {
+    const message = req.query.message;
+    if (!message) {
+      return res.status(400).json({ error: 'Parameter "message" tidak ditemukan' });
+    }
+    const response = await aikurumi(message);
+    res.status(200).json({
+      status: 200,
+      creator: "RiooXdzz",
+      data: { response }
+    });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+// Handle 404 error
+app.use((req, res, next) => {
+  res.status(404).send("Sorry can't find that!");
+});
+
+// Handle error
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
+// Jalankan server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+module.exports = app
