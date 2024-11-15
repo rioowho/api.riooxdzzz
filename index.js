6978a5779654cf791c479acf9d736d0c2ca9c121
RiooBjirr <188498615+rioowho@users.noreply.github.com>
2024-11-15 22:26:11 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -13,6 +13,16 @@ app.set("json spaces", 2);
 // Middleware untuk CORS
 app.use(cors());
 
+async function pinterest(message) {
+
+    let res = await fetch(`https://www.pinterest.com/resource/BaseSearchResource/get/?source_url=%2Fsearch%2Fpins%2F%3Fq%3D${message}&data=%7B%22options%22%3A%7B%22isPrefetch%22%3Afalse%2C%22query%22%3A%22${message}%22%2C%22scope%22%3A%22pins%22%2C%22no_fetch_context_on_resource%22%3Afalse%7D%2C%22context%22%3A%7B%7D%7D&_=1619980301559`);
+    let json = await res.json();
+    let data = json.resource_response.data.results;
+    if (!data.length) throw `Query "${message}" not found :/`;
+    return data[~~(Math.random() * data.length)].images.orig.url;
+
+}
+
 async function ttSearch(message) {
   return new Promise(async (resolve, reject) => {
     axios("https://tikwm.com/api/feed/search", {
@@ -340,6 +350,22 @@ app.get('/api/aikurumi', async (req, res) => {
   }
 });
 app.get('/api/search-tiktok', async (req, res) => {
+  try {
+    const message = req.query.message;
+    if (!message) {
+      return res.status(400).json({ error: 'Parameter "message" tidak ditemukan' });
+    }
+    const response = await ttSearch(message);
+    res.status(200).json({
+      status: 200,
+      creator: "RiooXdzz",
+      data: { response }
+    });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+app.get('/api/search-pinterest', async (req, res) => {
   try {
     const message = req.query.message;
     if (!message) {
